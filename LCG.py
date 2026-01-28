4e7ee14e7b4724038507450953602b1743b37f92
Julien3556 <julius.clairay@gmail.com>
2026-01-28 08:55:23 +0100

    LCG v1

---
+++ LCG.py
@@ -0,0 +1,18 @@
+def LCG(a, c, m, seed, nb_steps):
+    X = []
+    X_n = seed
+    for _ in range(nb_steps):
+        X_n = (a * X_n + c) % m
+        X.append(X_n)
+    return X
+
+if __name__ == "__main__":
+    # Example parameters for the LCG
+    a = 65539
+    c = 0
+    m = 2**31
+    seed = 7
+    nb_steps = 100
+
+    X = LCG(a, c, m, seed, nb_steps)
+    print(X)
