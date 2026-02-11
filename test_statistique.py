93bb25987a6da70fa16aff8442f0a2879c2801e9
Elliot <elliot.fauviau@gmail.com>
2026-02-11 08:24:56 +0100

    test_statistique V0

---
+++ test_statistique.py
@@ -0,0 +1,15 @@
+from kolmogorov_smirnov import *
+
+from Gen_systeme import *
+from BBS import * 
+from Box_Muller import *
+from HMAC_DRBG import *
+from LCG import *
+from Mersenne_Twister import *
+from XOR_NRGB import *
+
+print("Test avec Kolmogorov_smirnov")
+
+data = 
+
+test_kolmogorov_smirnov()
